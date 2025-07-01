c4ccdf45f3dc2b948ebdd7d623bf73b78fe9bc5b
Eman Tariq <emantariq.1004@gmail.com>
2025-07-01 16:42:33 +0500

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function calculateLoan() {
-  loanAmountValue = document.getElementById("loan-amount").value;
-  interestRateValue = document.getElementById("interest-rate").value;
-  MonthsToPayValue = document.getElementById("months-to-pay").value;
+  let loanAmountValue = document.getElementById("loan-amount").value;
+  let interestRateValue = document.getElementById("interest-rate").value;
+  let MonthsToPayValue = document.getElementById("months-to-pay").value;
 
-  interestToPayValue = loanAmountValue * interestRateValue * 0.01 / MonthsToPayValue;
+  let interest =
+    (loanAmountValue * interestRateValue * 0.01) / MonthsToPayValue;
+
+    monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2); //only 2 decimal places
+
+    document.getElementById("payment").innerHTML = `Monthly Payment: $${monthlyPayment}`;
 }
+
+
+window.onload = calculateLoan; //When the page loads, automatically run the calculateLoan() function.
